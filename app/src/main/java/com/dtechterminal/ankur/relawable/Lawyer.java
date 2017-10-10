bd3c1975e254d1bd7ca3a5d3f4beaa61c4e2af4c
drjaat <ranadhruvrana@gmail.com>
2017-10-11 01:54:26 +0530

    Lawyer Profile Updated

---
+++ app/src/main/java/com/dtechterminal/ankur/relawable/Lawyer.java
@@ -0,0 +1,13 @@
+package com.dtechterminal.ankur.relawable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lawyer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lawyer);
+    }
+}
